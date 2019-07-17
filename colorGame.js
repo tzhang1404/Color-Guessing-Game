8d01ec67d052ab68f7de89751eb576ada507bd3a
Tony Zhang <Johnson@dhcp-10-105-30-133.wireless.northwestern.private>
2019-07-17 15:08:27 -0500

    first upload

---
+++ colorGame.js
@@ -0,0 +1,107 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+
+function generateSixColor(){
+	var colorList = [];
+	for(var i = 0; i < 6; i++){
+		var color = "rgb(";
+		for(var j = 0; j < 3; j++){
+			var Limit = getRandomInt(0, 256);
+			if(j > 0){
+				color += " " + String(getRandomInt(0, 256));
+			}
+			else{
+				color += String(getRandomInt(0, 256));
+			}
+			if(j < 2){
+				color += ",";
+			}
+		}
+		color += ")";
+		colorList.push(color);
+	}
+
+	return colorList;
+}
+
+function assignColor(){
+	var colorList = generateSixColor();
+	var squareList = document.querySelectorAll(".square");
+	var answerColor = getRandomInt(0, 6);
+	squareList.forEach(function(square, index){
+		square.style.backgroundColor = colorList[index];
+	});
+
+	return colorList[answerColor];
+}
+
+function resetInterface(){
+	var squareList = document.querySelectorAll(".square");
+	var display = document.querySelector("#RGBColor");
+	var header = document.querySelector(".header");
+	display.textContent = "RGB";
+	header.style.backgroundColor = "";
+	squareList.forEach(function(square, index){
+		square.style.backgroundColor = "pink";
+		square.classList.remove("wrongChoice");
+		square.textContent = "";
+	});
+	var startButton = document.querySelector("#startButton");
+	startButton.textContent = "Start";
+	startButton.classList.remove("btn-danger");
+	started = false;
+
+}
+
+function startInterface(){
+	var answer = assignColor();
+	var display = document.querySelector("#RGBColor");
+	display.textContent = String(answer);
+	var startButton = document.querySelector("#startButton");
+	var header = document.querySelector(".header");
+	startButton.textContent = "Reset";
+	startButton.classList.add("btn-danger");
+	started = true;
+
+	//prepare for listening
+	var squareList = document.querySelectorAll(".square");
+	squareList.forEach(function(square, index){
+		square.addEventListener("click", function(){
+			if(started){
+				if(this.style.backgroundColor == answer){
+					header.style.backgroundColor = answer;
+					display.textContent = "Correct!";
+				}
+				else{
+					square.classList.add("wrongChoice");
+					square.textContent = "Wrong";
+				}
+			}
+			
+		});
+	});
+}
+
+function updateInterface(){
+	if(started){
+		resetInterface();
+	}
+	else{
+		startInterface();
+	}
+}
+
+
+//the game starts
+resetInterface();
+var started = false;
+var startButton = document.querySelector("#startButton");
+startButton.addEventListener("click", updateInterface);
+
+
+
+
